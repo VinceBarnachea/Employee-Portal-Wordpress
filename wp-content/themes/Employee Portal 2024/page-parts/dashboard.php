08e0225f1b757e27f90c4bfd42ba0c54a132a8b7
VinceBarnachea <barnacheatolentino@example.com>
2025-01-20 14:10:07 +0800

    Sidebar wireframe (2)

--- wp-content/themes/Employee Portal 2024/page-parts/dashboard.php
+++ wp-content/themes/Employee Portal 2024/page-parts/dashboard.php
@@ -20,7 +20,7 @@ include_once(get_template_directory() . '/controllers/connection.php');
 
 <main class="w-full h-full">
     <div class="grid grid-cols-12 h-full">
-        <div class="col-span-2 border-r-[1px] flex flex-col justify-between min-h-[100vh] px-6 py-8">
+        <div class="md:col-span-3 2xl:col-span-2 border-r-[1px] flex flex-col justify-between min-h-[100vh] px-6 py-8">
             <div class="w-full flex flex-col items-start justify-center">
                 <!-- Company Section -->
                 <div class="company-section flex items-center gap-3">
@@ -29,13 +29,7 @@ include_once(get_template_directory() . '/controllers/connection.php');
                 </div>
 
                 <!-- Current User -->
-                <div class="flex gap-3 justify-start items-center mt-12 px-3 py-2 border w-full">
-                    <img class = "object-cover aspect-[1/1] w-12 h-12" src="<?= get_template_directory_uri()?>/assets/images/img-wireframe.png" alt="" srcset="">
-                    <div class="flex flex-col items-start justify-center">
-                        <h2 class="text-base font-medium text-black"><?= "{$user_data['first_name']} {$user_data['last_name']}"?></h2>
-                        <span class="text-xs text-black"><?= $user_level === 'admin' ? 'Admin' : $user_data['position'];?></span>
-                    </div>
-                </div>
+                
 
 
                 <!-- Menus -->
@@ -73,24 +67,24 @@ include_once(get_template_directory() . '/controllers/connection.php');
                  </div>
 
 
-                 <div class="mt-24 w-full">
+                 <div class="mt-28 w-full">
                     <span class="text-xs text-black">Account</span>
-                    <div class="mt-4 flex flex-col gap-8">
-                        <div class="flex flex-row items-center gap-3 tab-menu hover:cursor-pointer transition-all duration-[300ms] ease-in-out">
-                            <img class = "object-cover aspect-[1/1] w-8 h-8" src="<?= get_template_directory_uri()?>/assets/images/img-wireframe.png" alt="" srcset="">
-                            <h2 class="text-base text-black">Profile</h2>
+                    <div class="flex gap-3 justify-start items-center mt-4 px-3 py-2 border w-full">
+                        <img class = "object-cover aspect-[1/1] w-12 h-12" src="<?= get_template_directory_uri()?>/assets/images/img-wireframe.png" alt="" srcset="">
+                        <div class="flex flex-col items-start justify-center">
+                            <h2 class="text-base font-medium text-black"><?= "{$user_data['first_name']} {$user_data['last_name']}"?></h2>
+                            <span class="text-xs text-black"><?= $user_level === 'admin' ? 'Admin' : $user_data['position'];?></span>
                         </div>
-                        
                     </div>
                  </div>
             </div> <!-- End of Menu -->
-            <button onclick="redirectTo('<?php echo $urlLogout; ?>');" class = "bg-red text-white py-2 px-6 text-md flex items-center gap-2 justify-center w-fit">
+            <button onclick="redirectTo('<?php echo $urlLogout; ?>');" class = "text-black flex items-center gap-2 justify-center w-fit">
                 <img class = "object-cover aspect-[1/1] w-6 h-6 invert filter" src="<?= get_template_directory_uri()?>/assets/images/img-wireframe.png" alt="" srcset="">
-                <h2 class="text-base">Logout</h2>
+                <h2 class="text-base">Log out</h2>
             </button>
         </div>
 
-        <div class="col-span-10">
+        <div class="md:col-span-9 2xl:col-span-10">
             
         </div>
     </div>
